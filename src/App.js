c9518d2263a4e2d14d868d97fd371364e5962836
YashasaveeKesarwani99 <56859852+YashasaveeKesarwani99@users.noreply.github.com>
2020-09-20 14:12:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import "leaflet/dist/leaflet.css";
+import IPtrackr from "./IPtrackr";
+
+export default function App() {
+  return (
+    <div className="App">
+      <IPtrackr />
+    </div>
+  );
+}
